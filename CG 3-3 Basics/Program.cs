2f05cfa4478df43a0791eb9421b72f985ce52e6f
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-27 22:06:40 -0500

    Comments add

--- CG 3-3 Basics/Program.cs
+++ CG 3-3 Basics/Program.cs
@@ -9,10 +9,16 @@ namespace CG_3_3_Basics
             //This is an expression
 
             double x;
+
+            //This is also a statement, telling you what the variable is.
             double y;
 
             x = 3;
             y = 1;
+
+            //This is another statement.  The operand would be the variable "double z"
+            //while operators would be the '=', and '/'.
+
             double z = y / x;
             Console.Write(z);
 
